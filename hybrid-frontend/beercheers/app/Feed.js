39b25cb51755b615ef54325f47b7d5cbabdcd333
andrespf02 <aepirela@miuandes.cl>
2024-11-18 18:43:32 -0300

    ver foto en feed

--- hybrid-frontend/beercheers/app/Feed.js
+++ hybrid-frontend/beercheers/app/Feed.js
@@ -1,31 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, Button, Image, StyleSheet, ActivityIndicator, TextInput } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image, StyleSheet, ActivityIndicator, TextInput } from 'react-native';
 import ActionCable from 'actioncable';
-import { MaterialIcons } from '@expo/vector-icons'; 
 import { getItem } from '../Storage';
 import axios from 'axios';
-import Icon from 'react-native-vector-icons/Ionicons';
 import { useRouter } from 'expo-router';
 import { BACKEND_URL } from '@env';
-
-// Ensure the WebSocket connection is authenticated
-const createAuthenticatedCable = async () => {
-  const token = (await getItem('authToken'))?.replace(/"/g, '');
-  const url = new URL(BACKEND_URL);
-  const BACKEND_IP = url.host;
-  return ActionCable.createConsumer(`ws://${BACKEND_IP}/cable?token=${token}`);
-};
 
 const Feed = ({ navigation }) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const router = useRouter();
   const [userId, setUserId] = useState(null);
-  const [error, setError] = useState('');
   const [subscribed, setSubscribed] = useState(false);
-  const [filter, setFilter] = useState(''); 
+  const [error, setError] = useState('');
+  const [filter, setFilter] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+  const [photos, setPhotos] = useState({}); // Estado para almacenar fotos por event_id
+
+  const router = useRouter();
+
   useEffect(() => {
     const fetchUserId = async () => {
       try {
@@ -45,10 +37,39 @@ const Feed = ({ navigation }) => {
       const token = storedToken ? storedToken.replace(/"/g, '') : null;
       setIsLoggedIn(!!token);
     };
-  
+
     checkLoginStatus();
   }, []);
 
+  // Función para obtener las fotos del evento
+  const fetchEventPhotos = async (eventId) => {
+    try {
+      const storedToken = await getItem('authToken');
+      const token = storedToken ? storedToken.replace(/"/g, '') : null;
+      const response = await axios.get(`${BACKEND_URL}/api/v1/events/${eventId}/event_pictures`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.data.data && Array.isArray(response.data.data)) {
+        const photoData = response.data.data;
+        setPhotos(prevPhotos => ({
+          ...prevPhotos,
+          [eventId]: photoData.map(photo => ({
+            id: photo.id,
+            description: photo.attributes.description,
+            url: photo.attributes.url,
+          })),
+        }));
+      }
+    } catch (error) {
+      console.error('Error fetching event photos:', error);
+    }
+  };
+
+  // Función para obtener las publicaciones iniciales
   const fetchInitialPosts = async () => {
     try {
       const storedToken = await getItem('authToken');
@@ -58,8 +79,17 @@ const Feed = ({ navigation }) => {
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log(response)
-      setPosts(response.data);
+
+      const postsWithImages = await Promise.all(
+        response.data.map(async (post) => {
+          if (post.event_id) {
+            // Llamamos a fetchEventPhotos para obtener las fotos relacionadas con el event_id
+            fetchEventPhotos(post.event_id);
+          }
+          return post;
+        })
+      );
+      setPosts(postsWithImages);
     } catch (error) {
       console.error('Error al cargar las publicaciones:', error);
       setError('Error al cargar las publicaciones. Inténtalo nuevamente.');
@@ -69,150 +99,89 @@ const Feed = ({ navigation }) => {
   };
 
   useEffect(() => {
-    createAuthenticatedCable().then((cable) => {
-      if (userId) {
-        const channel = cable.subscriptions.create(
-          { channel: 'FeedChannel', user_id: userId },
-          {
-            connected() {
-              console.log('Canal conectado');
-              setSubscribed(true);
-            },
-            received(data) {
-              if (data.type) {
-                console.log('Nuevo dato recibido:', data);
-                if (data.type === 'feed_photo') {
-                  setPosts((prevPosts) => [
-                    { ...data, picture: data.picture },
-                    ...prevPosts,
-                  ]);
-                } else {
-                  setPosts((prevPosts) => [
-                    { ...data, type: data.type },
-                    ...prevPosts,
-                  ]);
-                };
-              }
-            },
-          }
-        );
-  
-        return () => {
-          channel.unsubscribe();
-        };
-      }
-    });
-  }, [userId]);
-
-  useEffect(() => {
-    if (subscribed) {
-      fetchInitialPosts();
-    }
-  }, [subscribed, filter]);
-
-  const handleLogout = async () => {
-    router.push('/');
-  };
-
-  const handleLogin = () => {
-    router.push('/login');
-  };
-
-  const handleFilterChange = (text) => {
-    setFilter(text); 
     fetchInitialPosts();
-  };
+  }, []);
 
+  // Componente para renderizar cada item en el feed
+  const renderItem = ({ item }) => {
+    const eventPhotos = photos[item.event_id]; // Obtenemos las fotos para el event_id
+  
+    // Verificamos si la publicación es una "review" o una "picture"
+    const isReview = item.text && !item.event_id;  // Si tiene texto pero no un event_id, es una review
+    const isPicture = item.event_id; // Si tiene un event_id, es una picture
+  
+    return (
+      <View style={styles.postContainer}>
+        {isPicture && eventPhotos && eventPhotos.length > 0 && (
+          <View>
+            {/* Mostrar detalles específicos para picture */}
+            <Text style={styles.postTime}>{item.created_at}</Text> {/* Hora de la publicación */}
+            <Image source={{ uri: eventPhotos[0].url }} style={styles.postImage} />
+            {item.description && <Text style={styles.description}>{item.description}</Text>}
+  
+            {/* Etiquetas de usuarios en la foto */}
+            {item.tagged_users && item.tagged_users.length > 0 && (
+              <View style={styles.taggedUsersContainer}>
+                {item.tagged_users.map((user) => (
+                  <Text key={user.id} style={styles.taggedUser}>
+                    @{user.handle}
+                  </Text>
+                ))}
+              </View>
+            )}
+  
+            {/* Detalles del evento */}
+            {item.event_name && <Text style={styles.eventName}>{item.event_name}</Text>}
+            {item.bar_name && <Text style={styles.barName}>{item.bar_name}</Text>}
+            {item.country && <Text style={styles.country}>{item.country}</Text>}
+  
+            {/* Botón para ir al evento */}
+            <TouchableOpacity onPress={() => router.push(`/event/${item.event_id}`)} style={styles.eventButton}>
+              <Text style={styles.eventButtonText}>Ver Evento</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+  
+        {isReview && (
+          <View>
+            {/* Mostrar detalles específicos para review */}
+            {item.event_name && <Text style={styles.eventName}>{item.event_name}</Text>}
+            {item.beer_name && <Text style={styles.beerName}>{item.beer_name} Review</Text>}
+            {item.text && <Text style={styles.description}>{item.text}</Text>}
+            {item.rating && <Text style={styles.rating}>Rating: {item.rating}</Text>}
+          </View>
+        )}
+      </View>
+    );
+  };
+  
 
   if (loading) {
     return (
       <View style={styles.container}>
-        <Image source={require('../assets/icon_beercheers.png')} style={styles.icon} />
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size="large" color="#CFB523" />
-          <Text>Cargando publicaciones...</Text>
-        </View>
+        <ActivityIndicator size="large" color="#CFB523" />
+        <Text>Cargando publicaciones...</Text>
       </View>
     );
   }
-  
-  const renderItem = ({ item }) => {
-    // Si el item es una foto
-    if (item.event_picture_id) {
-      return (
-        <View style={styles.postContainer}>
-          <Text style={styles.eventName}>{item.event_name}</Text>
-          <Image source={{ uri: item.picture }} style={styles.postImage} />
-          <Text style={styles.description}>{item.description}</Text>
-        </View>
-      );
-    }
-    // Si el item es una reseña
-    else {
-      return (
-        <View style={styles.postContainer}>
-          <Text style={styles.beerName}>{item.beer_name} Review</Text>
-          <Text style={styles.description}>{item.text}</Text>
-          <Text style={styles.rating}>Rating: {item.rating}</Text>
-        </View>
-      );
-    }
-  };
 
   return (
     <View style={styles.container}>
-      <Image source={require('../assets/icon_beercheers.png')} style={styles.icon} />
-      
-      <TouchableOpacity
-        style={styles.logoutButton}
-        onPress={isLoggedIn ? handleLogout : handleLogin}
-      >
-        <Text style={styles.logoutButtonText}>{isLoggedIn ? 'Home' : 'Log In'}</Text>
-      </TouchableOpacity>
-
       <Text style={styles.title}>Feed</Text>
       {error && <Text style={styles.errorText}>{error}</Text>}
-      
+
       <TextInput
         style={styles.filterInput}
         placeholder="Filtrar por amistad, bar, país o cerveza"
         value={filter}
-        onChangeText={handleFilterChange}
-      />
-      
-      <FlatList
-        data={posts} 
-        renderItem={renderItem} 
-        keyExtractor={(item) => item.id.toString()}
+        onChangeText={setFilter}
       />
 
-      <View style={styles.bottomNavContainer}>
-        <View style={styles.bottomNavAction}>
-          <TouchableOpacity onPress={() => router.push('/BarsIndex')}>
-            <Image
-              source={require('../assets/baricon.png')}
-              style={styles.barIcon}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.bottomNavAction}>
-          <TouchableOpacity onPress={() => router.push('/beers')}>
-            <Image
-              source={require('../assets/searchgray.png')}
-              style={styles.searchIcon}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.bottomNavAction}>
-          <MaterialIcons name="map" size={24} color="#E3E5AF" onPress={() => router.push('/BarsIndexMap')} />
-        </View>
-        <View style={styles.bottomNavAction}>
-          <MaterialIcons name="person" size={24} color="#E3E5AF" onPress={() => router.push('/SearchUsers')} />
-        </View>
-        <View style={styles.bottomNavAction}>
-          <Icon name="list" size={24} color="#CFB523" onPress={() => router.push('/Feed')} />
-        </View>
-      </View>
+      <FlatList
+        data={posts}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
@@ -222,12 +191,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#303030',
   },
-  icon: {
-    width: 100,
-    height: 'auto',
-    marginBottom: 8,
-    alignSelf: 'center',
-  },
   title: {
     color: 'white',
     textAlign: 'center',
@@ -236,76 +199,14 @@ const styles = StyleSheet.create({
     fontSize: 32,
     textShadow: '1px 3px 3px black',
   },
-  photoItem: {
-    marginVertical: 8,
-    alignItems: 'center',
-  },
-  photoContainer: {
-    width: 150,
-    height: 150,
-    overflow: 'hidden',
-    borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-  },
-  eventPhoto: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-  },
-  errorText: { color: 'red' },
-  filterInput: { height: 40, borderColor: '#fff', borderWidth: 1, },
   postContainer: { marginBottom: 16 },
   eventName: { fontSize: 18, color: '#CFB523', fontWeight: 'bold' },
-  postImage: { width: '100%', height: 200, resizeMode: 'cover', marginVertical: 8 },
+  postImage: { width: '100%', height: 200, resizeMode: 'contain', marginVertical: 8 },
   beerName: { fontSize: 18, color: '#CFB523', fontWeight: 'bold' },
   rating: { fontSize: 14, color: '#888' },
-  description: {
-    color: 'white',
-    textAlign: 'center',
-    fontFamily: 'Roboto',
-  },
-  userHandles: {
-    color: '#CFB523',
-  },
-  bottomNavContainer: {
-    position: 'bottom',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    backgroundColor: '#303030',
-    borderTopWidth: 2,
-    borderTopColor: '#CFB523',
-    paddingVertical: 10,
-    marginTop: 20,
-  },
-  bottomNavAction: {
-    alignItems: 'center',
-  },
-  searchIcon: {
-    width: 32,
-    height: 26,
-  },
-  barIcon: {
-    width: 60,
-    height: 26,
-  },
-  logoutButton: {
-    position: 'absolute',
-    top: 40,
-    right: 20,
-    backgroundColor: '#CFB523',
-    padding: 10,
-    borderRadius: 5,
-    zIndex: 1,
-  },
-  logoutButtonText: {
-    color: 'white',
-    fontSize: 16,
-  },
+  description: { color: 'white', textAlign: 'center', fontFamily: 'Roboto' },
+  errorText: { color: 'red' },
+  filterInput: { height: 40, borderColor: '#fff', borderWidth: 1 },
 });
 
 export default Feed;
